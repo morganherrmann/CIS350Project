9f9e1715e922f4fc304bf2dc6dfa40b4041698f0
Morgan Herrmann <moher@seas.upenn.edu>
2019-03-14 14:45:06 -0400

    New files for home screen

---
+++ app/src/main/java/com/example/moher/cis350project/ParentHome.java
@@ -0,0 +1,13 @@
+package com.example.moher.cis350project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParentHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_home);
+    }
+}
