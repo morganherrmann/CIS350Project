e40b2732fc68ba4f37af6029a9f36536df4d25e6
aqfan <angelf@seas.upenn.edu>
2019-03-20 12:39:27 -0400

    added reading game activities

--- app/src/main/java/com/example/moher/cis350project/LoginPage.java
+++ app/src/main/java/com/example/moher/cis350project/LoginPage.java
@@ -25,6 +25,7 @@ public class LoginPage extends AppCompatActivity {
         Button student = findViewById(R.id.student);
         Button parent = findViewById(R.id.parent);
         Button math = findViewById(R.id.math);
+        Button reading = findViewById(R.id.reading);
 
         Intent current = getIntent();
         usernames = (HashMap) current.getSerializableExtra("data");
@@ -65,6 +66,16 @@ public class LoginPage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginPage.this, MathGame.class);
+                //intent.putExtra("data", usernames);
+                startActivity(intent);
+            }
+
+        });
+
+        reading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPage.this, ReadingGameMain.class);
                 //intent.putExtra("data", usernames);
                 startActivity(intent);
             }
---
+++ app/src/main/java/com/example/moher/cis350project/ReadingGame1.java
@@ -0,0 +1,14 @@
+package com.example.moher.cis350project;
+
+/**
+ * Created by Angel on 3/20/2019.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ReadingGame1 extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reading_game_level1);
+    }
+}
