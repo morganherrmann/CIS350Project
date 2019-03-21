320a4d744e90fdc05dc521d21ca635f2e2d178ec
aqfan <angelf@seas.upenn.edu>
2019-03-20 23:33:31 -0400

    finished alphabet song

--- app/src/main/java/com/example/moher/cis350project/ReadingGame1.java
+++ app/src/main/java/com/example/moher/cis350project/ReadingGame1.java
@@ -3,12 +3,28 @@ package com.example.moher.cis350project;
 /**
  * Created by Angel on 3/20/2019.
  */
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class ReadingGame1 extends AppCompatActivity{
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reading_game_level1);
+
+        // loads imageview with the animation
+        ImageView img = (ImageView)findViewById(R.id.alphabet_song);
+        img.setBackgroundResource(R.drawable.alphabet_song_animation);
+
+        // gets the background to be animated
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+        MediaPlayer ring= MediaPlayer.create(ReadingGame1.this,R.raw.alphabet_song);
+        ring.start();
+        // start the animation
+        frameAnimation.start();
     }
+
 }
